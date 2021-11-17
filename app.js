241e09009f2e2121f8219f219b5a75e803972a8f
Freizeit-sys <work16087y0rha@gmail.com>
2021-11-17 15:30:17 +0900

    refactoring app.js

--- app.js
+++ app.js
@@ -1,40 +1,64 @@
-const question = "ゲーム市場、最も売れたゲーム機は次の内どれ？";
-const answers = [
-  "スーパーファミコン",
-  "プレイステーション2",
-  "ニンテンドースイッチ",
-  "ニンテンドーDS",
+const quiz = [
+  {
+    question: 'ゲーム史上、最も売れたゲーム機はどれ？',
+    answers: ['スーパーファミコン',　'PlayStation 2',　'ニンテンドーDS',　'Xbox 360'],
+    correct: 'ニンテンドーDS',
+  },
+  {
+    question: '糸井重里が企画に関わった、任天堂の看板ゲームといえば？',
+    answers: ['MOTHER2', 'スーパーマリオブラザーズ3', 'スーパードンキーコング', '星のカービィ'],
+    correct: 'MOTHER2',
+  },
+  {
+    question: 'ファイナルファンタジーⅣの主人公の名前は？',
+		answers: ['フリオニール', 'クラウド', 'ティーダ', 'セシル'],
+    correct: 'セシル',
+  },
 ];
-const correct = "ニンテンドーDS";
+
+const quizLength = quiz.length;
+let quizIndex = 0;
+let score = 0;
 
 // HTMLのオブジェクトには、「$」マークを変数の先頭につける
-const $button = document.getElementsByTagName("button");
+const $button = document.getElementsByTagName('button');
 const buttonLength = $button.length;
 
 const setupQuiz = () => {
-  document.getElementById("js-question").textContent = "Question";
+  document.getElementById('js-question').textContent = quiz[quizIndex].question;
   let buttonIndex = 0;
   while (buttonIndex < buttonLength) {
-    $button[buttonIndex].textContent = answers[buttonIndex];
+    $button[buttonIndex].textContent = quiz[quizIndex].answers[buttonIndex];
     buttonIndex++;
   }
-};
+}
 
 setupQuiz();
 
 // ボタンにクリック時のイベントを追加
 const clickHandler = (e) => {
-  if (correct === e.target.textContent) {
-    window.alert("正解");
+	if (quiz[quizIndex].correct === e.target.textContent) {
+		window.alert('正解');
+		score++;
   } else {
-    window.alert("不正解");
-  }
-};
+    window.alert('不正解');
+	}
+	
+	quizIndex++
+
+	if (quizIndex < quizLength) {
+		// 問題数がまだあればこちらを実行
+		setupQuiz();
+	} else {
+		// 問題数がもうなければこちらを実行
+		window.alert('終了！あなたの正解数は' + score + '/' + quizLength + 'です！');
+	}
+}
 
 // ボタンをクリックしたら正誤判定
 let handlerIndex = 0;
 while (handlerIndex < buttonLength) {
-  $button[handlerIndex].addEventListener("click", (e) => {
+  $button[handlerIndex].addEventListener('click', (e) => {
     clickHandler(e);
   });
   handlerIndex++;
