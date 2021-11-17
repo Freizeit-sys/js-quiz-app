4093ceb6e1c861520f9f3f0857f30032d6a64897
Freizeit-sys <work16087y0rha@gmail.com>
2021-11-17 15:45:12 +0900

    refactoring complete-app.js

--- complete-app.js
+++ complete-app.js
@@ -25,3 +25,64 @@ const quiz = [
     correct: "セシル",
   },
 ];
+
+const $window = window;
+const $doc = document;
+const $question = $doc.getElementById('question-js');
+const $buttons = $doc.getElementsByTagName('btn');
+
+const quizLen = quiz.length;
+let quizCount = 0;
+let score = 0;
+
+const init = () => {
+	$question.textContent = quiz[quizCount].question;
+
+	const buttonLen = $buttons.length;
+	let buttonIndex = 0;
+
+	while (buttonIndex < buttonLen) {
+		$buttons[buttonIndex].textContent = quiz[quizCount].answers[buttonIndex];
+		buttonIndex++;
+	}
+};
+
+const goToNext = () => {
+	quizCount++;
+
+	if (quizCount < quizLen) {
+		init(quizCount);
+	} else {
+		// $window.alert('クイズ終了！');
+		showEnd();
+	}
+};
+
+const judge = (elm) => {
+	if (elm.textContent === quiz[quizCount].correct) {
+		$window.alert('正解！');
+		score++;
+	} else {
+		$window.alert('不正解！');
+	}
+	goToNext();
+};
+
+const showEnd = () => {
+	$question.textContent = "終了！あなたのスコアは" + score + "/" + quizLen + "です";
+
+	const $items = $doc.getElementById('js-item');
+	$items.style.visibility = 'hidden';
+};
+
+init();
+
+let answersIndex = 0;
+let answersLen = quiz[quizCount].answers.length;
+
+while (answersIndex < answersLen) {
+	$buttons[answersIndex].addEventListener('click', (e) => {
+		judge(e.target);
+	});
+	answersIndex++;
+};
